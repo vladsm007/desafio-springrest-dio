d904ba9431d94eb0d5b240dd5d5d326d6691debf
Victor Martins Torres <clerossephirot@gmail.com>
2024-09-16 09:47:38 -0300

    criação do repositorio

---
+++ src/main/java/com/vtorres/cadastro_cliente_dio/CadastroClienteDioApplication.java
@@ -0,0 +1,15 @@
+package com.vtorres.cadastro_cliente_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@SpringBootApplication
+public class CadastroClienteDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroClienteDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vtorres/cadastro_cliente_dio/CadastroClienteDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtorres.cadastro_cliente_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroClienteDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
